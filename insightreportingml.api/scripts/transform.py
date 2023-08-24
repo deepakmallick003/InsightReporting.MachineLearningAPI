425d7c9f03330ff35f3f3c28f1efc50976fa6a88
Deepak Mallick <d.Mallick@cabi.org>
2023-08-24 09:23:10 +0000

    Merged PR 2375: #60308, #60246 - Exporting Few Extra Columns, adding space and Updating Keywr...
    #60308, #60246 - Exporting Few Extra Columns, adding space and Updating Keywrods Logic
    
    Related work items: #60246, #60308

--- insightreportingml.api/scripts/transform.py
+++ insightreportingml.api/scripts/transform.py
@@ -1,9 +1,10 @@
 from . import extractkeywords
 from . import predictor
-
+import re
 
 def transform(data: dict, version='1'):
     eios_data_list = []
+    list_type_seperator = ';'
 
     if 'result' in data:
         json_results_arr = data["result"]
@@ -12,17 +13,17 @@ def transform(data: dict, version='1'):
                 ReportDate = item['pubDate'] if 'pubDate' in item else ''
                 Title = item['title']
                 ISO_Language = item['languageCode']
+                Language_Name = item['language']
                 psn_arr = [x for x in item['triggers'] if 'PHT' in x['key']]
-
-                Preferred_scientific_name = ';'.join([item['key'][2:item['key'].rfind(
-                    '-PHT')] for item in psn_arr if item['key'].startswith('l:') and item['key'].endswith('-PHT')])
+                Preferred_scientific_name = list_type_seperator.join(
+                    re.sub(r'([a-z])([A-Z])', lambda m: m.group(1) + ' ' + m.group(2), item['key'][2:item['key'].rfind('-PHT')])
+                    for item in psn_arr if item['key'].startswith('l:') and item['key'].endswith('-PHT')
+                )
 
                 desc = item['description'] if item['description'] else ""
                 desc_tran = item['translatedDescription'] if item['translatedDescription'] else ""
                 abs_sumry = item['abstractiveSummary'] if item['abstractiveSummary'] else ""
-                trgrs = sorted(set([value for item in item['triggers']
-                                    for value in item['values']]))
-                trgrs = ' '.join(trgrs)
+                
                 locations = item['locations']
                 area = locations[0]['areaFullName'] if locations else ""
                 area = ' '.join(area) if area else ""
@@ -39,27 +40,36 @@ def transform(data: dict, version='1'):
 
                 Results = item['link']
                 Website = item['source']['url']
-                Guid = item['rssItemId']
+                RssItemId = item['rssItemId']
+                EIOSItemId = item['id']
 
                 src = item.get("source", {})
                 source_keys = ["id", "name", "country",
-                               "region", "language", "subject"]
+                               "region", "language", "subject", "type"]
                 
-                Source, Source_Name, Source_Country, Source_Region, Source_Language, Source_Subject = (
+                Source, Source_Name, Source_Country, Source_Region, Source_Language, Source_Subject, Source_Type = (
                     ("" if value is None else value) for value in (src.get(key) for key in source_keys)
                 )
 
-                keywords = extractkeywords.extract_keywords(Merged_Texts)
+                Categories = '; '.join(tag["label"] for tag in item.get("tags", []) if tag.get("folders"))
+                MentionedCountries = '; '.join(country["label"] for country in item.get("affectedCountries", []))
 
-                score = predictor.predictor.predict([Title, ISO_Language, Preferred_scientific_name, GeoRss_Point,
+                trgr_seen = set()
+                trgrs = list_type_seperator.join([value.lower() for trigger in item['triggers'] for value in trigger['values'] if value.lower() not in (trgr_seen := trgr_seen or set()) and not trgr_seen.add(value.lower())])
+                keywords = extractkeywords.extract_keywords(Merged_Texts)
+                Merged_Keywords_Triggers = list_type_seperator.join(filter(None, [trgrs, keywords]))
+
+                Score = predictor.predictor.predict([Title, ISO_Language, Preferred_scientific_name, GeoRss_Point,
                                                      Source_Name, Source_Country, Source_Region, Source_Subject],
                                                     keywords, version)
                 eios_data_dict = {
-                    "ItemUniqueID": Guid,
+                    "ItemUniqueID": RssItemId,
+                    "EIOSItemID": EIOSItemId,
                     "Title": Title,
                     "ItemLink": Results,
                     "PublishedDate": ReportDate,
                     "ISOLanguageCode": ISO_Language,
+                    "Language_Name": Language_Name,
                     "GeoRss_Point": GeoRss_Point,
                     "SourceURL": Website,
                     "SourceId": Source,
@@ -68,9 +78,12 @@ def transform(data: dict, version='1'):
                     "SourceRegion": Source_Region,
                     "SourceLanguage": Source_Language,
                     "SourceSubject": Source_Subject,
+                    "SourceType": Source_Type,
                     "ScientificName": Preferred_scientific_name,
-                    "Keywords": keywords,
-                    "Accuracy": score,
+                    "Categories": Categories,
+                    "MentionedCountries": MentionedCountries,
+                    "Keywords": Merged_Keywords_Triggers,
+                    "Accuracy": Score,
                     "ModelVersionID": version
                 }
 
